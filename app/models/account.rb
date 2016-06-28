9375e1265d584d9a2d600aae3b8f7ca2ea91a88e
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-28 15:55:36 +0200

    Extracted calculation of total balance from view into account model, testdriven

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,11 @@
 class Account < ActiveRecord::Base
   has_many :deposits, dependent: :destroy
+
+  def getBalance
+    sum = 0.0;
+    self.deposits.each do |deposit|
+      sum += deposit.amount
+    end
+    return sum
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,11 +1,21 @@
 require "spec_helper"
 
 describe Account, type: :model do
+
   it { should have_many :deposits}
 
   it 'does not have any special validation requirements for the moment' do
     account = Account.new(name: "lol")
     expect(account).to be_valid
   end
-  
+
+  describe '#getBalance' do
+    let(:account) { Account.create name: 'MyAccount123' }
+
+    it 'returns the total current balance' do
+      account.deposits.create(amount: 150)
+      expect(account.getBalance).to eq 150
+    end
+  end
+
 end
