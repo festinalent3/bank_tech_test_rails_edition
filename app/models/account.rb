576bd8f20a83734db844bebfd96e36227a02674f
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-28 16:45:40 +0200

    refactored #getBalance

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,14 +1,26 @@
 class Account < ActiveRecord::Base
   has_many :deposits, dependent: :destroy
   has_many :withdrawals, dependent: :destroy
+  sum = 0.0
 
   def getBalance
-    sum = 0.0;
+    return getDeposits - getWithdrawals
+  end
+
+  private
+
+  def getDeposits
+    sum = 0
     self.deposits.each do |deposit|
       sum += deposit.amount
     end
+    return sum
+  end
+
+  def getWithdrawals
+    sum = 0
     self.withdrawals.each do |withdrawal|
-      sum -= withdrawal.amount
+      sum += withdrawal.amount
     end
     return sum
   end
